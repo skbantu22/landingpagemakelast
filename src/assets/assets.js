15566a0b17abdb44939b498552a42cb9d5a5e727
ontu <itsantu1@gmail.com>
2025-11-06 00:28:39 +0600

    Initial commit

---
+++ src/assets/assets.js
@@ -0,0 +1,44 @@
+import logo from './logo.png'
+import add_icon from './add_icon.png'
+import ads_photo from './ads.jpg'
+
+import order_icon from './order_icon.png'
+import upload_area from './upload_area.png'
+import parcel_icon from './parcel_icon.svg'
+import page1 from './Screenshot/page1.webp'
+import page2 from './Screenshot/page2.jpeg'
+import page3 from './Screenshot/page3.webp'
+import page4 from './Screenshot/page4.webp'
+import page5 from './Screenshot/page5.webp'
+import page6 from './Screenshot/page6.jpg'
+import page9 from './Screenshot/page9.jpg'
+import page10 from './Screenshot/page10.jpeg'
+import page11 from './Screenshot/page11.jpeg'
+import page12 from './Screenshot/page12.webp'
+import page13 from './Screenshot/page13.jpeg'
+import Product_img from './products.png'
+import photo2 from './Ads2.jpg'
+export const assets = {
+    logo,
+    add_icon,
+    order_icon,
+    upload_area,
+    parcel_icon,
+    page1,
+    page2 ,
+    page3,
+    page4,
+    page5,
+    page6,
+  
+    page9,
+    page10,
+    page11,
+    page12,
+    page13,
+   ads_photo,
+   photo2,
+   Product_img
+
+}
+
